9075a6ac7ed9aa86dc07b59aad29cf099f6280f6
Aman Bisht <aman.bisht@inventum.net>
2024-02-27 08:24:17 +0530

    temp commit

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,12 @@
-module.exports = {
-
-    globals: {
-        "ts-jest": {
-            isolatedModules: true,
-        },
+const jestConfig = {
+    verbose: true,
+    testURL: "http://localhost/",
+    'transform': {
+        '^.+\\.jsx?$': 'babel-jest',
     },
-};
+    testMatch: ['**/__tests__/*.js?(x)'],
+    preset: "@vue/cli-plugin-unit-jest",
+    transformIgnorePatterns: ["node_modules/(?!axios)"]
+}
+
+module.exports = jestConfig
--- src/App.test.js
+++ src/App.test.js
@@ -1,10 +1,12 @@
 import { signIn } from './utils/api';
+import axios from 'axios';
 jest.mock('axios');
 test('Api call for successful login', async () => {
-  axios.get.mockImplementation((url) => {
-    const userData = { username: "tobias.funke@reqres.in", password: 'abc' };
+  const userData = { username: "tobias.funke@reqres.in", password: 'abc' };
+
+  axios.post.mockImplementation((url) => {
     return Promise.resolve({ data: {}, status: 200 });
   });
-  const result = await signIn(userId);
+  const result = await signIn(userData);
   expect(result).toEqual({ status: "success" });
 });
